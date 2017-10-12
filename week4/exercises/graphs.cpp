59ecee271931e0886156d2c579a82840e58b517e
Jakub Lichman <lichman.jakub@gmail.com>
2017-10-12 20:33:26 +0200

    Week4: Graphs intro problem solved

--- week4/exercises/graphs.cpp
+++ week4/exercises/graphs.cpp
@@ -7,39 +7,66 @@
 //BGLincludes
 #include<boost/graph/adjacency_list.hpp>
 #include<boost/graph/strong_components.hpp>
-#include<boost/graph/dijkstra_shortest_paths.hpp> //Namespaces
+#include<boost/graph/dijkstra_shortest_paths.hpp> 
+#include<boost/graph/kruskal_min_spanning_tree.hpp>
 
 using namespace std; 
 using namespace boost;
 
 //Directed graph with integer weights on edges. 
-typedef adjacency_list<vecS, vecS, directedS,
+typedef adjacency_list<vecS, vecS, undirectedS,
 no_property,
 property<edge_weight_t, int> > Graph;
-typedef graph_traits<Graph>::vertex_descriptor Vertex; //Vertex type 
-typedef graph_traits<Graph>::edge_descriptor Edge; //Edgetype
-typedef graph_traits<Graph>::edge_iterator EdgeIt;     // Edge iterator 
-//Property map edge->weight
-typedef property_map<Graph,edge_weight_t>::type WeightMap;
+typedef graph_traits<Graph>::vertex_descriptor Vertex;      //Vertex type 
+typedef graph_traits<Graph>::edge_descriptor Edge;          //Edgetype
+typedef graph_traits<Graph>::edge_iterator EdgeIt;          // Edge iterator 
+typedef property_map<Graph,edge_weight_t>::type WeightMap;  //Property map edge->weight
 
 void testcase(){
-    // Read and build graph
-    int V, E, t; // 1st line: <vertex_no> <edge_no> <target>
-    cin >> V >> E >> t;
-    Graph GT(V); // Creates an empty graph on V vertices
+    int V, E; 
+    cin >> V >> E;
+
+    Graph GT(V);
     WeightMap weightmap = get(edge_weight, GT);
+
     for (int i = 0; i < E; ++i) {
-        int u, v, w; // Each edge: <from> <to> <weight>
+        int u, v, w; 
         cin >> u >> v >> w;
         Edge e; 
-        bool success; // *** We swap u and v to create ***
+        bool success; 
 
-        tie(e,success) = add_edge(v,u,GT);//***thereversedgraphGT! ***
+        tie(e,success) = add_edge(v,u,GT);
         weightmap[e] = w;
     }
+
+    vector<Edge> mst;                                       // Vector to store MST edges (not a property map!) 
+    kruskal_minimum_spanning_tree(GT, back_inserter(mst)); 
+    vector<Edge>::iterator ebeg, eend = mst.end();
+
+    int weights = 0;
+    for(ebeg=mst.begin(); ebeg != eend; ++ebeg){
+        int w = boost::get(boost::edge_weight_t(), GT, *ebeg);
+        weights += w;
+    }
+
+    vector<int> distances(V);
+    vector<Vertex> predecessors(V);
+
+    Vertex v = 0;
+    dijkstra_shortest_paths(GT, 0,
+        predecessor_map(make_iterator_property_map(predecessors.begin(), get(vertex_index, GT))).
+        distance_map(make_iterator_property_map(distances.begin(), get(vertex_index, GT))));
+        int max = INT_MIN;
+        for(int i = 0; i < V; i++){
+            if(distances[i] > max && distances[i] < INT_MAX)
+                max = distances[i];
+        }
+
+    cout << weights << " " << max << endl;
 }
 
 int main(){
+    ios_base::sync_with_stdio(false);
     int n;
     cin >> n;
     for(int i = 0; i < n; i++){
