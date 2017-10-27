d8dd64873dd13f2e6c2e08d29b6b04e27b74ebe0
Jakub Lichman <lichman.jakub@gmail.com>
2017-10-27 16:42:31 +0200

    Light pattern 100/100

--- week5/exercises/light_pattern.cpp
+++ week5/exercises/light_pattern.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 #include<climits>
+#include<algorithm>
+#include<cassert>
 
 using namespace std;
 
+// returns how many bulbs are wrong in interval <from, from + k)
 int check_bulbs(bool* bulbs, bool* pattern, int k, int from){
     int wrong = 0;
     for(int i = from; i < from + k; i++){
@@ -12,6 +15,13 @@ int check_bulbs(bool* bulbs, bool* pattern, int k, int from){
     return wrong;
 }
 
+// corrects number of wrong according to no of global switches
+int correct_wrong(int switches, int switched, int k){
+    if(switched % 2 == 1)
+        return k - switches;
+    return switches;
+}
+
 void solve(){
     int n, k, x;
     cin >> n >> k >> x;
@@ -19,43 +29,36 @@ void solve(){
     bool* bulbs = new bool[n];
 
     for(int i = 1; i <= k; i++){
-        pattern[k - i] = (1 << (i - 1)) & x;
+        pattern[k - i] = (1 << (i - 1)) & x;        // read pattern
     }
 
     for(int i = 0; i < n; i++)
-        cin >> bulbs[i];
+        cin >> bulbs[i];                            // read bulbs
 
-    int global_single_switches = 0, global_all_switches = 0, local_single_switches, local_all_switches;
+    int global_single_switches = 0, global_switches_needed = 0, next_wrong, wrong;
     int groups = n/k;
-    int switches_needed[groups];
-    int switch_sum = 0;
-    int n_of_switches = 0;
 
-    for(int i = 0; i < groups; i++){
-        switches_needed[i] = check_bulbs(bulbs, pattern, k, i * k);
-        switch_sum += switches_needed[i];
-    }
-    switch_sum -= switches_needed[groups - 1];
+    for(int i = groups - 1; i > 0; i--){
+        wrong = correct_wrong(check_bulbs(bulbs, pattern, k, i * k), global_switches_needed, k);                // read how many are wrong in current group
+        next_wrong = correct_wrong(check_bulbs(bulbs, pattern, k, (i - 1) * k), global_switches_needed, k);     // read how many are wrong in next group
 
-    for(int i = groups - 1; i >= 0; i--){
-        if(switch_sum == 0)
-            break;
+        if(wrong == 0)                                                                                          // if no are wrong than continue
+            continue;
 
-        if(switch_sum > ((i + 1) * k / 2)){
-            switch_sum = 0;
-            for(int j = 0; j < groups; j++){
-                switches_needed[j] = k - switches_needed[j];
-                switch_sum += switches_needed[j];
-            }
-            n_of_switches += switches_needed[i] + 1;
-        }
-        else {
-            n_of_switches += switches_needed[i];
-            switch_sum -= switches_needed[i];
+        if((wrong > (k/2) && next_wrong > (k/2)) || (k > 1 && wrong > ((k + 1)/2))){                            // if I can by global switch switch more bulbs into correct position than do it
+            global_switches_needed++;
+            if(k > 1)
+                global_single_switches += k - wrong;                                                            // switch the ones than were wrong after global switch in the group
+        } else {
+            global_single_switches += wrong;                                                                    // otherwise switch individually
         }
     }
 
-    cout << n_of_switches << endl;
+    int lastGroup = correct_wrong(check_bulbs(bulbs, pattern, k, 0), global_switches_needed, k);                // deal with the last group(first in the order)
+    if(k > 1)
+        lastGroup = min(lastGroup, k - lastGroup + 1);
+
+    cout << global_single_switches + global_switches_needed + lastGroup << endl;
 }
 
 int main(){
@@ -66,68 +69,3 @@ int main(){
     }
     return 0;
 }
-
-
-
-/*1 1 1 0 0 1 1 1
-1 1 1 0 0 1 1 1
-
-sum = 6 i = 7
-switches = 0
-if(6 > 4){
-    global_switch;
-    count again;
-    update sum;
-    switches += 1 + 0
-}
-
-0 0 0 1 1 0 0 0
-0 0 0 1 1 0 0 0
-
-sum = 2 i = 6
-switches = 1
-if(2 > 3){
-    nothing
-} else {
-    switches += local_single_switches // 0
-}
-
-0 0 0 1 1 0 0 0
-0 0 0 1 1 0 0 0
-
-sum = 2 i = 5
-switches = 1
-if(2 > 3){
-    nothing
-} else {
-    switches += local_single_switches // 0
-}
-
-0 0 0 1 1 0 0 0
-0 0 0 1 1 0 0 0
-
-sum = 2 i = 4
-switches = 1
-if(2 > 2){
-    nothing
-} else {
-    switches += local_single_switches // 1
-}
-
-0 0 0 1 1 0 0 0
-0 0 0 1 1 0 0 0
-
-sum = 1 i = 3
-switches = 2
-
-if(1 > 2){
-    nothing
-} else {
-    switches += local_single_switches // 1
-}
-
-sum = 0 i = 2
-switches = 3
-
-sum == 0 we are done
-*/
