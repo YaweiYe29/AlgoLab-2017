bdf1bcc1e4160c76b15bb9f2d043fb7a09395724
Jakub Lichman <lichman.jakub@gmail.com>
2018-01-11 18:13:10 +0100

    Marathon 100/100, Lestrade 50/50

--- week6/exercises/marathon.cpp
+++ week6/exercises/marathon.cpp
@@ -3,15 +3,29 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <algorithm>
+#include <climits>
+#include <cassert>
 
 // BGL includes
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
 #include <boost/graph/push_relabel_max_flow.hpp>
 #include <boost/graph/edmonds_karp_max_flow.hpp>
-#include <boost/graph/dijkstra_shortest_paths.hpp>
-// Namespaces
-// using namespace std;
-using namespace boost;
+
+// BGL Graph definitions
+// =====================
+// Graph Type, OutEdgeList Type, VertexList Type, (un)directedS
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,		// Use vecS for the VertexList! Choosing setS for the OutEdgeList disallows parallel edges.
+		boost::no_property,				// interior properties of vertices	
+		boost::property<boost::edge_weight_t, int> 		// interior properties of edges
+		>					Graph2;
+typedef boost::graph_traits<Graph2>::edge_descriptor		Edge2;		// Edge Descriptor: an object that represents a single edge.
+typedef boost::graph_traits<Graph2>::vertex_descriptor		Vertex2;		// Vertex Descriptor: with vecS vertex list, this is really just an int in the range [0, num_vertices(G)).	
+typedef boost::graph_traits<Graph2>::edge_iterator		EdgeIt2;		// to iterate over all edges
+typedef boost::graph_traits<Graph2>::out_edge_iterator		OutEdgeIt2;	// to iterate over all outgoing edges of a vertex
+typedef boost::property_map<Graph2, boost::edge_weight_t>::type	WeightMap;	// property map to access the interior property edge_weight_t
+
 
 // BGL Graph definitions
 // =====================
@@ -27,16 +41,7 @@ typedef	boost::property_map<Graph, boost::edge_residual_capacity_t>::type	Residu
 typedef	boost::property_map<Graph, boost::edge_reverse_t>::type		ReverseEdgeMap;
 typedef	boost::graph_traits<Graph>::vertex_descriptor			Vertex;
 typedef	boost::graph_traits<Graph>::edge_descriptor			Edge;
-
-//Undirected graph with integer weights on edges and parallel edges. 
-typedef adjacency_list<vecS, vecS, directedS,
-allow_parallel_edge_tag,
-property<edge_weight_t, int> > Graph2;
-typedef graph_traits<Graph2>::vertex_descriptor Vertex2;      //Vertex type 
-typedef graph_traits<Graph2>::edge_descriptor Edge2;          //Edgetype
-typedef graph_traits<Graph2>::edge_iterator EdgeIt;          // Edge iterator 
-typedef property_map<Graph2,edge_weight_t>::type WeightMap;  //Property map edge->weight
-
+typedef	boost::graph_traits<Graph>::edge_iterator			EdgeIt;
 
 // Custom Edge Adder Class, that holds the references
 // to the graph, capacity map and reverse edge map
@@ -65,71 +70,61 @@ public:
 	}
 };
 
-// Functions
-// =========
-// Function for an individual testcase
-void testcases() {
+void testcase() {
     int n, m, start, end, from, to, length, capacity;
     std::cin >> n >> m >> start >> end;
-	// Create Graph and Maps
+
     Graph Gfinal;
+    Graph2 G(n);
+    WeightMap weightmap = boost::get(boost::edge_weight, G);
 
 	EdgeCapacityMap capacitymap = boost::get(boost::edge_capacity, Gfinal);
 	ReverseEdgeMap revedgemap = boost::get(boost::edge_reverse, Gfinal);
 	ResidualCapacityMap rescapacitymap = boost::get(boost::edge_residual_capacity, Gfinal);
 	EdgeAdder eaG(Gfinal, capacitymap, revedgemap);
-    
-    Graph2 Gtmp(n);
-	Graph2 Gtmp2(n);
-    WeightMap weightmap = boost::get(edge_weight, Gtmp);
-	WeightMap weightmap2 = boost::get(edge_weight, Gtmp2);
-	std::map<Edge2, int> capMap;
 
+    std::map<Edge2, int> capacities;
     for(int i = 0; i < m; i++){
 		std::cin >> from >> to >> capacity >> length;
-		std::cout << "adding edge from " << from << " to " << to << std::endl;
-		bool success;
-		Edge2 e1, e2;
-		boost::tie(e1, success) = boost::add_edge(from, to, Gtmp);	
-		boost::tie(e2, success) = boost::add_edge(to, from, Gtmp2);
-		weightmap[e1] = length;	
-		weightmap2[e2] = length;	
-		capMap[e1] = capacity;
+		bool success; Edge2 e;
+		boost::tie(e, success) = boost::add_edge(from, to, G);	
+        //std::cout << "Adding edge from " << from << " to " << to << " with length " << length << std::endl;
+		weightmap[e] = length;
+        capacities[e] = capacity;
 	}
 
    	// Dijkstra shortest paths
 	// =======================
-	std::vector<int> distmap(n);		// We will use this vector as an Exterior Property Map: Vertex -> Distance to source
-	dijkstra_shortest_paths(Gtmp, start, // We MUST provide at least one of the two maps
-		distance_map(make_iterator_property_map(distmap.begin(), get(vertex_index, Gtmp))));	// distance map as Named Parameter
+	std::vector<int> distmap(n);		
+	boost::dijkstra_shortest_paths(G, start, 
+		boost::distance_map(boost::make_iterator_property_map(distmap.begin(), boost::get(boost::vertex_index, G))));
 
-	std::vector<int> distmap2(n);		// We will use this vector as an Exterior Property Map: Vertex -> Distance to source
-	dijkstra_shortest_paths(Gtmp2, end, // We MUST provide at least one of the two maps
-		distance_map(make_iterator_property_map(distmap2.begin(), get(vertex_index, Gtmp2))));	// distance map as Named Parameter
+	std::vector<int> distmap2(n);
+	dijkstra_shortest_paths(G, end,
+		boost::distance_map(boost::make_iterator_property_map(distmap2.begin(), boost::get(boost::vertex_index, G))));
 
 	assert(distmap[end] == distmap2[start]);
-	std::cout << "min distance: " << distmap[end] << std::endl;
-	EdgeIt ebeg, eend;	
-	for (tie(ebeg, eend) = edges(Gtmp); ebeg != eend; ++ebeg) {	// edges(G) returns a pair of iterators which define a range of all edges. 
-		// For undirected graphs, each edge is visited once, with some orientation.
-		// ebeg is EdgeIterator, *ebeg is EdgeDescriptor
-		Vertex2 u = source(*ebeg, Gtmp);
-		Vertex2 v = target(*ebeg, Gtmp);
+    if(distmap[end] == 0 || distmap[end] == INT32_MAX){
+        std::cout << 0 << std::endl;
+        return;
+    }
 
-		int cap = capMap[*ebeg];
+	EdgeIt2 ebeg, eend;	
+	for (boost::tie(ebeg, eend) = boost::edges(G); ebeg != eend; ++ebeg) {
+		Vertex u = source(*ebeg, G);
+		Vertex v = target(*ebeg, G);
+
+		int cap = capacities[*ebeg];
 		int len = weightmap[*ebeg];
-		if(len + distmap[u] + distmap2[v] == distmap[end] || len + distmap[u] + distmap2[v] == distmap[end]){
-			std::cout << "adding edge from " << u << " to " << v << std::endl;
-			eaG.addEdge(u, v, cap);
-		}
+		if(len + distmap[u] + distmap2[v] == distmap[end]){
+		    eaG.addEdge(u, v, cap);
+		} else if (len + distmap2[u] + distmap[v] == distmap[end]){
+            eaG.addEdge(v, u, cap);
+        }
 	}
 
 	// Calculate flow
-	// If not called otherwise, the flow algorithm uses the interior properties
-	// - edge_capacity, edge_reverse (read access),
-	// - edge_residual_capacity (read and write access).
     long flow = boost::push_relabel_max_flow(Gfinal, start, end);
-
     std::cout << flow << std::endl;
 }
 
@@ -139,7 +134,7 @@ int main() {
     int n;	
     std::cin >> n;
 	while(n--){
-        testcases();
+        testcase();
     }
 	return 0;
 }
--- week7/exercises/lestrade2.cpp
+++ week7/exercises/lestrade2.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
-#include <chrono>
 
 #include <CGAL/Gmpq.h>
 #include <CGAL/basic.h>
@@ -23,9 +22,9 @@ typedef Triangulation::Edge_iterator  Edge_iterator;
 
 typedef struct GangMember{
     int x, y, u, v, w;
+    int bestA;
 } GangMember;
 
-using namespace std::chrono;
 void testcase(){
     // z -> money requested by holmes
     // u -> 'where' required
@@ -41,76 +40,53 @@ void testcase(){
 
     // gang members
     std::map<K::Point_2, GangMember> gang;
-    std::map<K::Point_2, int> spiedGangs;
     std::vector<K::Point_2> pts;
     pts.reserve(g);
     Triangulation t;
-    int tmpU = 0, tmpV = 0, tmpW = 0;
     // load gang members
     for(int i = 0; i < g; i++){
         GangMember gg;
         std::cin >> gg.x >> gg.y >> gg.u >> gg.v >> gg.w;
+        gg.bestA = -1;
         K::Point_2 pt(gg.x, gg.y);
         gang[pt] = gg;
         pts.push_back(pt);
     }
-
-    high_resolution_clock::time_point t1 = high_resolution_clock::now();
     t.insert(pts.begin(), pts.end());
-    
-    high_resolution_clock::time_point t2 = high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
-    std::cout << duration << std::endl;
     // our variables will be hours of agents so they have to be between 0 and 24
     Program * lp = new Program(CGAL::LARGER, true, 0, true, 24);
 
     lp->set_b(0, u);
     lp->set_b(1, v);
     lp->set_b(2, w);
-t1 = high_resolution_clock::now();
+
     for(int i = 0; i < a; i++){
         int x, y, z; // z -> hour wage, x, y position
         std::cin >> x >> y >> z;
         // find closest gang member
         K::Point_2 p = t.nearest_vertex(K::Point_2(x, y))->point();
-        if(spiedGangs[p] == 0)
-            spiedGangs[p] = z;
+        int gg = gang[p].bestA;
+        if(gg == -1)
+            gang[p].bestA = z;
         else 
-            spiedGangs[p] = std::min(spiedGangs[p], z);
+            gang[p].bestA = std::min(gg, z);
     }
-    t2 = high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
-    std::cout << duration << std::endl;
     
-t1 = high_resolution_clock::now();
     int agent = 0;
-    for(auto it = spiedGangs.begin(); it != spiedGangs.end(); ++it){
-        int z = it->second;
-        GangMember gg = gang[it->first];
-        lp->set_a(agent, 0, gg.u);
-        lp->set_a(agent, 1, gg.v);
-        lp->set_a(agent, 2, gg.w);
-        tmpU += gg.u * 24;
-        tmpV += gg.v * 24;
-        tmpW += gg.w * 24;
-        lp->set_c(agent, z);
-        agent++;
-    }
-t2 = high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
-    std::cout << duration << std::endl;
-    if(tmpU < u || tmpV < v || tmpW < w){
-        std::cout << "H\n";
-        return;
+    for(auto it = gang.begin(); it != gang.end(); ++it){
+        GangMember gg = it->second;
+        int z = gg.bestA;
+        if(z != -1){
+            lp->set_a(agent, 0, gg.u);
+            lp->set_a(agent, 1, gg.v);
+            lp->set_a(agent, 2, gg.w);
+            lp->set_c(agent, z);
+            agent++;
+        }
     }
 
-t1 = high_resolution_clock::now();
-    //std::cout << "agents: " << agent << "(real: " << a << ")" << std::endl;
     // solve it!
     Solution s = CGAL::solve_linear_program(*lp, ET());
-t2 = high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
-    std::cout << duration << std::endl;
     if(s.is_infeasible()){
         std::cout << "H\n";
     } else {
