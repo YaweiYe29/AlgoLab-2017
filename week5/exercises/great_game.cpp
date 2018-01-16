8b919e64511b04292d6e555aa5b3351a698c5745
Jakub Lichman <lichman.jakub@gmail.com>
2018-01-16 20:24:16 +0100

    The great game 75/100

--- week5/exercises/great_game.cpp
+++ week5/exercises/great_game.cpp
@@ -1,76 +1,74 @@
+/* solution worth 75 points*/
 #include <iostream>
 #include <vector>
+#include <climits>
+#include <algorithm>
 
-using namespace std;
+// memo first dimension is 0 (for even moves) and 1 for odd moves
+std::vector<std::vector<int>> memo;
 
-vector<int>* positions;
-int holmes_turns;
-int moriarty_turns;
-bool printed;
+int play(int move, int b, std::vector<std::vector<int>>& graph, int n){
+    if(b == n - 1)                              // if we reached end return n of moves
+        return move;
 
-void turn(int red_position, int black_position, bool sherlock_move, int n){
-    cout << "(" << red_position << "," << black_position << ")\n";
-    if(red_position == n /*&& !printed*/){
-        cout << "0\n";
-        printed = true;
-        return;
+    if(memo[move%2][b] != -1)                   // if memo contains value return it
+        return memo[move%2][b] + move;
+
+    int result;
+    if(move % 2 != 0){                          // sherlock -> black, moriarty -> red
+        int maxB = 0;
+        for(auto it = graph[b].begin(); it != graph[b].end(); ++it){    // try all paths from current node and pick the maximal one
+            int moves = play(move + 1, *it, graph, n);
+            maxB = std::max(moves, maxB);
+        }
+        result = maxB;
+    } else { // sherlock -> red, moriarty -> black
+        int minB = INT32_MAX;                                           // try all paths from current node and pick the minimal one
+        for(auto it = graph[b].begin(); it != graph[b].end(); it++){
+            int moves = play(move + 1, *it, graph, n);
+            minB = std::min(moves, minB);
+        }
+        result = minB;
     }
-
-    if(black_position == n /*&& !printed*/){
-        cout << "1\n";
-        printed = true;
-        return;
-    }
-
-
-    if(sherlock_move){
-        if(holmes_turns % 2 == 0){
-            holmes_turns++;
-            for(int i = 0; i < positions[red_position].size(); i++)
-                turn(positions[red_position][i], black_position, !sherlock_move, n);
-        }
-        else {
-            holmes_turns++;
-            for(int i = 0; i < positions[black_position].size(); i++)
-                turn(red_position, positions[black_position][i], !sherlock_move, n);
-        }
-
-    } else {
-        if(moriarty_turns % 2 == 0){
-            moriarty_turns++;
-            for(int i = 0; i < positions[black_position].size(); i++)
-                turn(red_position, positions[black_position][i], !sherlock_move, n);
-        }
-        else {
-            moriarty_turns++;
-            for(int i = 0; i < positions[red_position].size(); i++)
-                turn(positions[red_position][i], black_position, !sherlock_move, n);
-        }
-    }
+    memo[move%2][b] = result - move;            // save to memo
+    return result;
 }
 
-void play(){
-    holmes_turns = 0;
-    moriarty_turns = 0;
-    printed = false;
-    
-    // red, black meeple | even - holmes(red) -moriarty(black) | holmes starts
-    int n, m, r, b, from, to;
-    cin >> n >> m >> r >> b;
-    positions = new vector<int>[n];
-    for(int i = 0; i < m; i++){
-        cin >> from >> to;
-        positions[from - 1].push_back(to - 1);
+void testcase(){
+    int n, m, r, b;
+    std::cin >> n >> m >> r >> b;
+    memo = std::vector<std::vector<int>>(2, std::vector<int>(n, -1));
+
+    std::vector<std::vector<int>> graph(n);
+    for(int i = 0; i < m; i++){                 // load graph
+        int from, to;
+        std::cin >> from >> to;
+        graph[from-1].push_back(to-1);
+    }
+    int movesB = play(0, b-1, graph, n);        // find best strategy for moriarty
+    int movesR = 0, curr = r-1;
+    while(curr != n - 1){                       // find number of moves for red meeple
+        movesR++;
+        curr = graph[curr][0];
     }
 
-    turn(r - 1, b - 1, true, n - 1);
+    if(movesB < movesR)                         // if moriarty reaches n-1 first he is the winner
+        std::cout << "1";
+    else if(movesB > movesR)                    // otherwise sherlock
+        std::cout << "0";
+    else {                                      // if both at the same time than depend on whether move is even or odd
+        if(movesR % 2 != 0)
+            std::cout << "0";
+        else 
+            std::cout << "1";
+    }
+    std::cout << std::endl;
 }
 
 int main(){
-    std::ios_base::sync_with_stdio(false);
-    int n;
-    cin >> n;
-    while(n--){
-        play();
-    }
+    int t;
+    std::cin >> t;
+    while(t--)
+        testcase();
+    return 0;
 }
