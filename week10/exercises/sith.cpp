edcbb1169f5cbcaaf66a5f6eeec0cf2e629c0a4f
Jakub Lichman <lichman.jakub@gmail.com>
2018-01-18 15:49:05 +0100

    Sith 100/100

--- week10/exercises/sith.cpp
+++ week10/exercises/sith.cpp
@@ -4,68 +4,73 @@
 #include <map>
 #include <set>
 #include <algorithm>
+#include <cassert>
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_2.h>
+
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/connected_components.hpp>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef CGAL::Delaunay_triangulation_2<K>  Triangulation;
 typedef Triangulation::Edge_iterator  Edge_iterator;
 typedef Triangulation::Edge Edge;
+typedef Triangulation::Vertex_handle Vertex_handle;
 
-std::map<K::Point_2, std::vector<K::Point_2> > reachables;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, boost::no_property>	Graph;
 
-int recur(K::Point_2 vertex, std::set<K::Point_2> visited){
-    int max = 1;
-    visited.insert(vertex);
-    for (int i = 0; i < reachables[vertex].size(); i++){
-        if(visited.find(reachables[vertex][i]) == visited.end()){
-            //std::cout << vertex << " " << reachables[vertex][i] << std::endl;
-            int res = recur(reachables[vertex][i], visited) + 1;
-            if(res > max)
-                max = res;
-        }
+// finds max component in nearest neighbor graph 
+int max_component(std::map<K::Point_2,int> ptsi, std::vector<K::Point_2> pts, int f, long long rr, int n){
+    Triangulation t;
+    t.insert(pts.begin() + f, pts.end());
+
+    Graph G(n - f);
+    for (auto edgeit = t.finite_edges_begin(); edgeit != t.finite_edges_end(); ++edgeit) {
+        Edge edge = *edgeit;
+        K::Point_2 v1 = t.segment(edge).source();
+        K::Point_2 v2 = t.segment(edge).target(); 
+        if(CGAL::squared_distance(v1, v2) <= rr)
+            boost::add_edge(ptsi[v1] - f, ptsi[v2] - f, G);
     }
-    visited.erase(vertex);
-    return max;
+    std::vector<int> componentmap(n - f);
+	int ncc = boost::connected_components(G, boost::make_iterator_property_map(componentmap.begin(), boost::get(boost::vertex_index, G))); 
+	std::vector<int> componentsize(ncc);
+	for (int i = 0; i < n-f; ++i)
+		++componentsize[componentmap[i]];
+	return *std::max_element(componentsize.begin(), componentsize.end());
 }
 
 void testcase(){
     int n, r;
     std::cin >> n >> r;
-    r = r * r;
+    long rr = r;
+    rr *= rr;
+
     // read points
     std::vector<K::Point_2> pts;
+    std::map<K::Point_2, int> ptsi;
     pts.reserve(n);
-    reachables.clear();
-
     for (int i = 0; i < n; i++) {
         K::Point_2 p;
         std::cin >> p;
-        if(i >= n / 2)
-            pts.push_back(p);
+        pts.push_back(p);
+        ptsi[p] = i;
     }
 
-    for (int i = 0; i < pts.size(); i++){
-        std::cout << pts[i] << ": ";
-        for (int j = i + 1; j < pts.size(); j++){
-            if (CGAL::squared_distance(pts[i], pts[j]) <= r){
-                std::cout << "(" << pts[j] << ") ";
-                reachables[pts[i]].push_back(pts[j]);
-                reachables[pts[j]].push_back(pts[i]);
-            }
-        }
-        std::cout << std::endl;
+    int low = 0;
+    int high = n / 2 + 1;
+    // binary search over (0, n/2 + 1) to find appropriate k
+    while(high - low > 1){
+        int k = low + (high - low) / 2;
+        int res = max_component(ptsi, pts, k, rr, n);
+        if(k > res)
+            high = k;
+        else
+            low = k;
     }
 
-    int max = 0;
-    //std::cout << pts.size() << std::endl;
-    for(int i = 0; i < pts.size(); i++) {
-        int res = recur(pts[i], std::set<K::Point_2>());
-        if(res > max)
-            max = res;
-    }
-    std::cout << max << std::endl;
+    std::cout << low << std::endl;
 }
 
 int main(){
@@ -75,6 +80,5 @@ int main(){
     while(n--) {
         testcase();
     };
-
     return 0;
 }
