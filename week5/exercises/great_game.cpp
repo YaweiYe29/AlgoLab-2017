6f461a0126ae5b5f248a2565dfbef67cae3b2aa6
Jakub Lichman <lichman.jakub@gmail.com>
2018-02-05 14:51:53 +0100

    Great Game reimplemented

--- week5/exercises/great_game.cpp
+++ week5/exercises/great_game.cpp
@@ -1,74 +1,61 @@
-/* solution worth 75 points*/
 #include <iostream>
 #include <vector>
-#include <climits>
 #include <algorithm>
 
-// memo first dimension is 0 (for even moves) and 1 for odd moves
-std::vector<std::vector<int>> memo;
+using namespace std;
+// even -> Mor(b), Holmes(r)
+// odd -> Mor(r), Holmes(b)
 
-int play(int move, int b, std::vector<std::vector<int>>& graph, int n){
-    if(b == n - 1)                              // if we reached end return n of moves
-        return move;
-
-    if(memo[move%2][b] != -1)                   // if memo contains value return it
-        return memo[move%2][b] + move;
-
+vector<vector<int>> memo;                           // memo first dimension is 0 (for even moves) and 1 for odd moves
+int move(int moves, int meeple, int n, vector<vector<int>> & graph){
+    if(meeple == n-1)                               // if we reached end return n of moves
+        return moves;
+    
+    if(memo[moves%2][meeple] != -1)                 // if memo contains value return it
+        return memo[moves%2][meeple] + moves;
     int result;
-    if(move % 2 != 0){                          // sherlock -> black, moriarty -> red
-        int maxB = 0;
-        for(auto it = graph[b].begin(); it != graph[b].end(); ++it){    // try all paths from current node and pick the maximal one
-            int moves = play(move + 1, *it, graph, n);
-            maxB = std::max(moves, maxB);
+    if(moves % 2 != 1){                             // both want to minimize on their even move
+        int min_moves = INT32_MAX;                  // try all paths from current node and pick the maximal one
+        for(auto it = graph[meeple].begin(); it != graph[meeple].end(); ++it){
+            min_moves = min(min_moves, move(moves+1, *it, n, graph));
         }
-        result = maxB;
-    } else { // sherlock -> red, moriarty -> black
-        int minB = INT32_MAX;                                           // try all paths from current node and pick the minimal one
-        for(auto it = graph[b].begin(); it != graph[b].end(); it++){
-            int moves = play(move + 1, *it, graph, n);
-            minB = std::min(moves, minB);
+        result = min_moves;
+    } else {                                        // both want to maximize on their odd move
+        int max_moves = INT32_MIN;                  // try all paths from current node and pick the minimal one
+        for(auto it = graph[meeple].begin(); it != graph[meeple].end(); ++it){
+            max_moves = max(max_moves, move(moves+1, *it, n, graph));
         }
-        result = minB;
+        result = max_moves;
     }
-    memo[move%2][b] = result - move;            // save to memo
+    memo[moves%2][meeple] = result - moves;         // save to memo
     return result;
 }
 
 void testcase(){
     int n, m, r, b;
-    std::cin >> n >> m >> r >> b;
-    memo = std::vector<std::vector<int>>(2, std::vector<int>(n, -1));
-
-    std::vector<std::vector<int>> graph(n);
-    for(int i = 0; i < m; i++){                 // load graph
+    cin >> n >> m >> r >> b;
+    r--; b--;
+    vector<vector<int>> graph(n);
+    memo = vector<vector<int>>(2, vector<int>(n, -1));
+    for(int i = 0; i < m; i++){
         int from, to;
-        std::cin >> from >> to;
-        graph[from-1].push_back(to-1);
+        cin >> from >> to;
+        from--; to--;
+        graph[from].push_back(to);
     }
-    int movesB = play(0, b-1, graph, n);        // find best strategy for moriarty
-    int movesR = 0, curr = r-1;
-    while(curr != n - 1){                       // find number of moves for red meeple
-        movesR++;
-        curr = graph[curr][0];
-    }
-
-    if(movesB < movesR)                         // if moriarty reaches n-1 first he is the winner
-        std::cout << "1";
-    else if(movesB > movesR)                    // otherwise sherlock
-        std::cout << "0";
-    else {                                      // if both at the same time than depend on whether move is even or odd
-        if(movesR % 2 != 0)
-            std::cout << "0";
-        else 
-            std::cout << "1";
-    }
-    std::cout << std::endl;
+    int moves_r = move(0, r, n, graph);
+    int moves_b = move(0, b, n, graph);
+    if(moves_r < moves_b) cout << "0" << endl;      // when there are less moves to red than sherlock wins
+    else if(moves_r > moves_b) cout << "1" << endl; // otherwise moriarty
+    else if(moves_r % 2 != 0) cout << "0" << endl;  // if num. of moves is equal. Than we need to check with which figure 
+    else cout << "1" << endl;                       // Holmes moved in last move. If with red than he won otherwise Moriarty.
 }
 
 int main(){
     int t;
-    std::cin >> t;
-    while(t--)
+    cin >> t;
+    while(t--){
         testcase();
+    }
     return 0;
 }
